2b5551521a3b6764e6b9b4d0f3245bfd88158218
murali-cse <muralicse97@gmail.com>
2023-03-30 12:30:00 +0530

    theme updated

---
+++ precache-manifest.f8251ddaa5c55b5d94774720c4efaad7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "06f883e71054109b6d8b",
+    "url": "/static/css/main.fd6bb5ce.chunk.css"
+  },
+  {
+    "revision": "06f883e71054109b6d8b",
+    "url": "/static/js/main.48cccd4e.chunk.js"
+  },
+  {
+    "revision": "ea633294823f2c840e95",
+    "url": "/static/js/runtime~main.f6a04f92.js"
+  },
+  {
+    "revision": "7607af7375a6971317c0",
+    "url": "/static/js/2.0f6c7f05.chunk.js"
+  },
+  {
+    "revision": "7dd329510d223992ae45",
+    "url": "/static/js/3.f3a4d0fe.chunk.js"
+  },
+  {
+    "revision": "817b4a56b047d0d36b338dd9ffd44dbd",
+    "url": "/static/media/flutter.817b4a56.svg"
+  },
+  {
+    "revision": "f0793d9f85eb3f0d1c702c14d236ba9e",
+    "url": "/static/media/react.f0793d9f.svg"
+  },
+  {
+    "revision": "c675a4b60c6281c9a296c3c811aaced3",
+    "url": "/static/media/node.c675a4b6.svg"
+  },
+  {
+    "revision": "faa96d91067ceb47ab0d356780c8b32d",
+    "url": "/static/media/ci.faa96d91.svg"
+  },
+  {
+    "revision": "c3562bb4a70b31762fe71f48cfe2efac",
+    "url": "/static/media/ai.c3562bb4.svg"
+  },
+  {
+    "revision": "4411fe4a47dd0772bd959652f5b45c34",
+    "url": "/static/media/website.4411fe4a.png"
+  },
+  {
+    "revision": "59a1666b52d41f17c1a5049b4e6c79b2",
+    "url": "/static/media/graphic_design.59a1666b.png"
+  },
+  {
+    "revision": "81b7c6563910c8d1ff72e2dd41544451",
+    "url": "/static/media/process.81b7c656.png"
+  },
+  {
+    "revision": "0de7b23acf7fd07c5aef62a34c55f07e",
+    "url": "/static/media/logo_filled.0de7b23a.svg"
+  },
+  {
+    "revision": "2b4c94310dbd9f04d04e124a04bbcd86",
+    "url": "/static/media/logo.2b4c9431.svg"
+  },
+  {
+    "revision": "760a46191e9826b9b5137290671e62ba",
+    "url": "/index.html"
+  }
+];
